e74526d2107fd523f9dcfe3db47ac850132ec86c
sunil0336 <sunilrathod.0336@gmail.com>
2025-04-16 01:39:26 +0530

    small update

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="min-h-screen bg-gray-900 text-white pb-2 pt-20">
       <Nav />
-      <div className="max-w-4xl mx-auto bg-gray-800 p-6 rounded-lg shadow-lg p-8 mt-2 ">
+      <div className="max-w-4xl mx-auto bg-gray-800  rounded-lg shadow-lg p-8 mt-2 ">
         <h1 className="text-3xl font-bold mb-4">About the Min-Max Algorithm</h1>
 
         <section className="mb-6">
